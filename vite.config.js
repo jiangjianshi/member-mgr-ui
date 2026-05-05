2505d72055983cfa969614045e2f26672be8ea62
harris <jiangjianshi001@163.com>
2026-05-05 15:38:12 +0800

    修改外网可以访问

--- vite.config.js
+++ vite.config.js
@@ -8,5 +8,10 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+  server: {
+    host: '0.0.0.0', // 允许外部访问
+    port: 5173,      // 指定端口号（可选）
+    open: true       // 自动打开浏览器（可选）
   }
 })
